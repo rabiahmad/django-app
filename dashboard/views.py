13fa3431c8d995453b60fd3e8deadbf2f11497aa
Rabi Ahmad <rabihassanahmad@gmail.com>
2023-04-05 13:15:25 +0100

    changed line chart dimension to month-year rather than month

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,6 +11,7 @@ from datetime import datetime, date
 import pandas as pd
 import calendar
 
+
 class DashboardView(TemplateView):
     template_name = "dashboard/index.html"
 
@@ -23,38 +24,37 @@ class DashboardView(TemplateView):
         return context
 
     def _total_sales(self):
-        """ Get transactions data from database and calculate total sales """
+        """Get transactions data from database and calculate total sales"""
         total_sales_agg = Transaction.objects.aggregate(Sum("sales"))
         total_sales = "${:,}".format(int(total_sales_agg["sales__sum"]))
         return total_sales
-    
+
     def _total_orders(self):
-        """ Get total number of orders from transactions dataset """
+        """Get total number of orders from transactions dataset"""
         total_orders_agg = Transaction.objects.aggregate(Count("order_id"))
         total_orders = int(total_orders_agg["order_id__count"])
         return total_orders
-    
+
     def _monthly_sales_by_date(self):
-        """ Get sum of sales by order date for chart """
+        """Get sum of sales by order date for chart"""
         labels = []
         data = []
 
+        # data processing
         df = pd.DataFrame(list(Transaction.objects.all().values()))
-        df["month"] = pd.to_datetime(df["order_date"]).dt.month
-        df = df.groupby("month").sum().reset_index()
+        df["order_date"] = pd.to_datetime(df["order_date"])
+        df["month_year"] = df["order_date"].dt.to_period("M")
+        df = df.groupby("month_year").sum().reset_index()
 
-        labels = [calendar.month_abbr[x] for x in df["month"].values]
+        # returning the chart labels and data
+        labels = list(df["month_year"].astype(str).values)
         data = list(df["sales"].values)
-        
-        context = {
-            "labels": labels,
-            "data": data
-        }
-        
+        context = {"labels": labels, "data": data}
+
         return context
-        
+
     def _sales_by_category(self):
-        """ Get sales by category """
+        """Get sales by category"""
         labels = []
         data = []
 
@@ -63,31 +63,11 @@ class DashboardView(TemplateView):
 
         labels = list(df["category"].values)
         data = list(df["sales"].values)
-        
-        context = {
-            "labels": labels,
-            "data": data
-        }
-        
+
+        context = {"labels": labels, "data": data}
+
         return context
 
-# class LineChartData(APIView):
-
-#     def get(self, request, format=None):
-#         qs = Transaction.objects.filter().values("order_date").order_by("order_date").annotate(sales=Sum("sales"))
-#         labels = []
-#         data = []
-        
-#         for item in qs:
-#             labels.append(item.get("order_date"))
-#             data.append(item.get("sales"))
-        
-#         context = {
-#             "labels": labels,
-#             "data": data
-#         }
-#         return JsonResponse(context)
-    
 
 def tables(request):
     return render(request, "dashboard/tables.html")
